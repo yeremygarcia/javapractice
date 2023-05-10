b0eb0a682af68807c38f7745f3320b34aa59fe2c
yeremygarcia <jeremyisgarcia@gmail.com>
2023-05-09 22:25:21 -0400

    1

---
+++ MyNameIs.java
@@ -0,0 +1,10 @@
+
+public class MyNameIs {
+    public static void main(String[] args) {
+        System.out.println("My name is");
+
+        System.out.println("Larry");
+
+        System.out.println("Sprinkle");
+    }
+}
---
+++ Vacation.java
@@ -0,0 +1,9 @@
+public class Vacation {
+    public static void main (String[] args) {
+        System.out.println("I'd like to visit");
+
+        System.out.println("Tokyo");
+
+        System.out.println("Japan");
+    }
+}
